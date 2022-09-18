3521b276a4651649bc4f5aa487e5c217682cc089
mwala <mwalamulenga.zt@gmail.com>
2022-09-18 14:32:04 +0200

    Created models and url for user model CRUD ops

--- cuddle/cuddle/urls.py
+++ cuddle/cuddle/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from feels import urls as cuddle_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cuddle/', include(cuddle_url) ),
+    path('api-auth/', include('rest_framework.urls')),
 ]
