9b3174eaff9d23f40ac1e9d97d8aeb32c7818285
Mwala Zechariah Mulenga <66120792+mwala-zm@users.noreply.github.com>
2024-01-23 15:59:31 +0200

    Delete cuddle directory
    Restarting project

--- cuddle/feels/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from feels.models import User, Movie, Music
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Movie)
-admin.site.register(Music)
--- cuddle/feels/apis/spotify.py
+++
@@ -1,1 +0,0 @@
-# Spotify api integration
