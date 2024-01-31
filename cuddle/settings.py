96d63d3b67ff7098007c479aa706b81e427f9140
mwala-zm <mwalamulenga.zt@gmail.com>
2024-01-31 09:15:13 +0200

    Add cors for api requests

--- cuddle/settings.py
+++ cuddle/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_countries",
     "phonenumber_field",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "cuddle.urls"
 
