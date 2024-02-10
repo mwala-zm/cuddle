cc5041535344081f50f1e60dfd297f9fd534bae9
mwala-zm <mwalamulenga.zt@gmail.com>
2024-02-10 15:31:04 +0200

    Add Quotes to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { MovieList, Spotify } from "./components";
+import styles from "./styles";
 
 function App() {
   return (
-    <div className="bg-background px-[16px] overflow-hidden font-satoshi">
+    <div
+      className={`${styles.paddingX}bg-background px-[16px] overflow-hidden font-satoshi w-full h-screen`}
+    >
+      <div className={`${styles.padding}`}>
+        <h2 className={`${styles.heading2}`}>Quotes</h2>
+        <ul>
+          <li>In the Air</li>
+        </ul>
+      </div>
       <div>
         <MovieList />
       </div>
